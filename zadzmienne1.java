2b4494591e517aaf111d8365d3fdd560d14c529a
WiktorMurzyn <50379769+WiktorMurzyn@users.noreply.github.com>
2019-05-16 21:17:19 +0200

    Add files via upload

---
+++ zadzmienne1.java
@@ -0,0 +1,15 @@
+public class zmienne {
+
+    public static void main(String[] args) {
+        String marka = "Honda";
+        String model = "Civic";
+        int rocznik = 2018;
+        double cena = 120000.0;
+
+        System.out.println("Wybrany samochud to "+ marka);
+        System.out.println("Model wybranego samochuodu to "+ model);
+        System.out.println("Rocznik samochodu to "+ rocznik);
+        System.out.println("Cena samochodu to "+ cena);
+
+    }
+}
